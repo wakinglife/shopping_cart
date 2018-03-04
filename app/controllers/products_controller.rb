8461023b2b91d6b3a47e5de7e904a44026abe988
fion chen <wakinglife_615@yahoo.com>
2018-03-05 00:13:18 +0800

    visitor can read product index and detail

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < ApplicationController
+  class ProductsController < ApplicationController
+    def index
+      @products = Product.page(params[:page]).per(50)
+    end
+
+    def show
+      @product = Product.find(params[:id])
+    end
+
+  end
+
 end
