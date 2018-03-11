6554026731b03a72f2371fb860c96533b3e57dc3
fion chen <wakinglife_615@yahoo.com>
2018-03-11 21:22:35 +0800

    as order created, send email to user

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,36 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = current_user.orders.order(created_at: :desc)
+  end
+
+   def create
+     # manually check if user logged in
+     if current_user.nil?
+       # store order data in session so we can retrieve it later
+       session[:new_order_data] = params[:order]
+       # redirect to devise login page
+       redirect_to new_user_session_path
+     else
+       @order = current_user.orders.new(order_params)
+       @order.sn = Time.now.to_i
+       @order.add_order_items(current_cart)
+       @order.amount = current_cart.subtotal
+       if @order.save
+         current_cart.destroy
+         session.delete(:new_order_data)
+        UserMailer.notify_order_create(@order).deliver_now!
+         redirect_to orders_path, notice: "new order created"
+       else
+         @items = current_cart.cart_items
+         render "carts/show"
+       end
+     end
+   end
+
+   private
+
+   def order_params
+     params.require(:order).permit(:name, :phone, :address, :payment_method)
+   end
+
+end
