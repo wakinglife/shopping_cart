a337f55e54bb0875318c8134f8b5f7390fc95c8c
fion chen <wakinglife_615@yahoo.com>
2018-03-28 19:45:45 +0800

    user can cancel order before shipped

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -24,6 +24,15 @@ class Admin::OrdersController < ApplicationController
      end
    end
 
+
+   def update
+    @order = current_user.orders.find(params[:id])
+    if @order.shipping_status == "not_shipped"
+      @order.shipping_status = "cancelled"
+      @order.save
+      redirect_to orders_path, alert: "order##{@order.sn} cancelled."
+    end
+  end
    private
 
    def order_params
