8b158d5dfe03abbd3c42cdd2e1f5644aab805d59
fion chen <wakinglife_615@yahoo.com>
2018-03-28 16:40:37 +0800

    fix wording

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ApplicationMailer
-  default from: "My Cart <info@alphacamp.co>"
+  default from: "My Cart <info@myshop.co>"
 
    def notify_order_create(order)
      @order = order
      @content = "Your order is created. Thank you!"
 
      mail to: order.user.email,
-     subject: "ALPHA Camp | 訂單成立: #{@order.id}"
+     subject: "My Shop | 訂單成立: #{@order.id}"
    end
 
 end
