4b21d7367ff88009caecd328b88e2b1c0e188048
fion chen <wakinglife_615@yahoo.com>
2018-03-29 05:33:18 +0800

    add notify url

--- app/controllers/spgateway_controller.rb
+++ app/controllers/spgateway_controller.rb
@@ -2,39 +2,27 @@ class SpgatewayController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def return
-    # 比對回傳的 SHA 和我們自行加密的 SHA 是否一樣
-
-    trade_info = spagatway_params['TradeInfo']
-    trade_sha = spagatway_params['TradeSha']
-
-    # 若 SHA 驗證通過，將智付通回傳的 TradeInfo 解密，取得參數內容
-
-    data = Spgateway.decrypt(trade_info, trade_sha)
-    # 根據參數的 MerchantOrderNo，查出 payment 實例
-    # 更新相關的 payment 與 order 屬性
-    if data
-       payment = Payment.find(data['Result']['MerchantOrderNo'].to_i)
-       if params['Status'] == 'SUCCESS'
-         payment.paid_at = Time.now
-       end
-       payment.params = data
-     end
-
+     payment = Payment.find_and_process(spagatway_params)
      if payment&.save
-        order = payment.order
-        order.update(payment_status: "paid")
         # send paid email
         flash[:notice] = "#{payment.sn} paid"
       else
         flash[:alert] = "Something wrong!!!"
       end
-
     # 動作完成，導回訂單索引頁
     redirect_to orders_path
    end
 
-   private
+   def notify
+       payment = Payment.find_and_process(spagatway_params)
+       if payment&.save
+         render text: "1|OK"
+       else
+         render text: "0|ErrorMessage"
+       end
+     end
 
+   private
 
   # 取出必要參數
   def spagatway_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'spgateway/return'
-
+  post "spgateway/notify"
+  
   root "products#index"
     resources :products, only: [:index, :show] do
       post :add_to_cart, on: :member
